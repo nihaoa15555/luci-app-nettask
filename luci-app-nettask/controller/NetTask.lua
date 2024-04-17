a4d223eb16ae7c019e9463c66985e68e0470c3d2
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-04-17 16:25:09 +0800

    Update NetTask.lua

--- luci-app-nettask/controller/NetTask.lua
+++ luci-app-nettask/controller/NetTask.lua
@@ -1,7 +1,7 @@
 module("luci.controller.NetTask", package.seeall)
 
 function index()
-	entry({"admin", "system", "NetTask"},alias("admin", "system", "NetTask","commonly"), _("自定义脚本"))
-	entry({"admin", "system", "NetTask", "commonly"},cbi("nettask"), _("基本功能"), 10).leaf = true
-	entry({"admin", "system", "NetTask", "trontabs"}, cbi("trontabs"), _("高级功能"), 20).leaf = true
+	entry({"admin", "services", "NetTask"},alias("admin", "services", "NetTask","commonly"), _("网页登入脚本"))
+	entry({"admin", "services", "NetTask", "commonly"},cbi("nettask"), _("基本功能"), 10).leaf = true
+	entry({"admin", "services", "NetTask", "trontabs"}, cbi("trontabs"), _("高级功能"), 20).leaf = true
 end
